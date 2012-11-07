33df9f69e673784e5af5ff31b7d57b2ce7326e81
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-11-08 05:31:47 +0700

    Add get_software_info(), add some more data, tweaks

--- lib/Software/Catalog.pm
+++ lib/Software/Catalog.pm
@@ -8,9 +8,14 @@ use Perinci::Sub::Gen::AccessTable 0.17 qw(gen_read_table_func);
 
 require Exporter;
 our @ISA       = qw(Exporter);
-our @EXPORT_OK = qw(list_software);
+our @EXPORT_OK = qw(
+                       get_software_info
+                       list_software
+               );
 
 # VERSION
+
+our %SPEC;
 
 # XXX import catalog from software-catalog
 # (https://github.com/sharyanto/software-catalog)
@@ -18,62 +23,118 @@ my @software = (
     {
         id           => 'wordpress',
         debian_names => [qw/wordpress/],
-        lang         => 'php',
-        tags         => [qw/webapp/],
+        tags         => [qw/
+                               implemented-in::php
+                               interface::web
+                               web::blog
+                           /],
+    },
+    {
+        id           => 'joomla',
+        debian_names => undef,
+        tags         => [qw/
+                               implemented-in::php
+                               interface::web
+                               web::cms
+                           /],
+    },
+    {
+        id           => 'jquery',
+        debian_names => [qw/libjs-jquery/],
+        tags         => [qw/
+                               implemented-in::javascript
+                               devel::library
+                           /],
+    },
+    {
+        id           => 'nginx',
+        debian_names => [qw/nginx/],
+        tags         => [qw/
+                               implemented-in::c
+                               interface::daemon
+                               protocol::http
+                           /],
     },
 );
 
 my $deb_re = qr/\A[a-z0-9]+(-[a-z0-9]+)*\z/;
-my $tag_re = $deb_re;
+my $tag_re = qr/\A([a-z0-9]+(-[a-z0-9]+)*::)?[a-z0-9]+(-[a-z0-9]+)*\z/x;
+
+my $table_spec = {
+    fields => {
+        id => {
+            index      => 0,
+            schema     => ['str*' => {
+                match => $deb_re,
+            }],
+            searchable => 1,
+        },
+        debian_names => {
+            index      => 2,
+            schema     => ['array' => {
+                of => ['str*' => {
+                    match => $deb_re,
+                }],
+            }],
+            sortable   => 0,
+        },
+        tags => {
+            index      => 2,
+            schema     => ['array' => {
+                of => ['str*' => match => $tag_re],
+            }],
+            sortable   => 0,
+        },
+        # XXX field: summary (in various languages)
+        # XXX field: description (in various languages)
+        # XXX field: license
+        # XXX field: url
+
+        # for download_url and releases/latest release, see
+        # Software::Release::Watch
+    },
+    pk => 'id',
+};
 
 my $res = gen_read_table_func(
     name => 'list_software',
     table_data => \@software,
-    table_spec => {
-        fields => {
-            id => {
-                index      => 0,
-                schema     => ['str*' => {
-                    match => $deb_re,
-                }],
-                searchable => 1,
-            },
-            debian_names => {
-                index      => 2,
-                schema     => ['array*' => {
-                    of => ['str*' => {
-                        match => $deb_re,
-                    }],
-                }],
-                sortable   => 0,
-            },
-            tags => {
-                index      => 2,
-                schema     => ['array*' => {
-                    of => ['str*' => match => $tag_re],
-                }],
-                sortable   => 0,
-            },
-            # XXX field: lang
-            # XXX field: summary (in various languages)
-            # XXX field: description (in various languages)
-            # XXX field: license
-            # XXX field: url
-            # XXX field: download_url (see Software::Release::Watch)
-        },
-        pk => 'id',
-    },
+    table_spec => $table_spec,
     langs => ['en_US'],
 );
 die "BUG: Can't generate func: $res->[0] - $res->[1]"
     unless $res->[0] == 200;
+
+$SPEC{get_software_info} = {
+    summary => 'Get info on a software',
+    args => {
+        id => {
+            summary  => $table_spec->{fields}{summary},
+            schema   => $table_spec->{fields}{schema},
+            req      => 1,
+            pos      => 0,
+        },
+    },
+};
+sub get_software_info {
+    my %args = @_;
+    my $id = $args{id}; # VALIDATE_ARG
+
+    my $res = list_software("id" => $id, detail=>1);
+    return [404, "No such software"] unless @{$res->[2]};
+
+    [200, "OK", $res->[2][0]];
+}
+
+# XXX get_software_XXX_info (if later on we need more specific info, or when
+# retrieving all info becomes heavy).
 
 1;
 # ABSTRACT: Software catalog
 
 =head1 SYNOPSIS
 
- use Software::Catalog qw(list_software);
+ use Software::Catalog qw(list_software get_software_info);
  my $res = list_software();
 
 
